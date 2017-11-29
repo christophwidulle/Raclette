a1dd66207d2fd736b6b7cefe29e6943c014c0293
Jurma Jacobsen <jurma.jacobsen@chefkoch.de>
2017-11-29 10:12:28 +0100

    Use better docker image for android sdk environment in ci/build_and_push_to_artifactory.sh
    * No hassle with Licence Agreements

--- ci/build_and_push_to_artifactory.sh
+++ ci/build_and_push_to_artifactory.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-# Requires 3 arguments:
-#   Example from bamboo:
+# Requires 3 arguments
+#   Example invocations
+#     From bamboo:
 #       build_and_push_to_artifactory.sh ${bamboo.artifactory.url} ${bamboo.artifactory.username} ${bamboo.artifactory.password}
+#     Local with Docker installed - use correct values for artifactoryUsername and artifactoryPassword
+#       ./ci/build_and_push_to_artifactory.sh "http://artifactory.prod.chefkoch.de:30046/artifactory/libs-snapshot-local" "artifactoryUsername" "artifactoryPassword"
 set -x
 
 artifactoryUrl=$1
 artifactoryUsername=$2
 artifactoryPassword=$3
 
-docker run --rm -v $(pwd):/builddir -w /builddir dr.chefkoch.net/android-build/base "mkdir /opt/android-sdk-linux/licenses/; echo -e \n8933bad161af4178b1185d1a37fbf41ea5269c55 > /opt/android-sdk-linux/licenses/android-sdk-license; ./gradlew clean build && ./gradlew publish -Partifactory_url=$artifactoryUrl -Partifactory_user=$artifactoryUsername -Partifactory_password=$artifactoryPassword"
+# https://github.com/mindrunner/docker-android-sdk
+docker run --rm -v "$(pwd)":/builddir -w /builddir runmymind/docker-android-sdk bash -xc "./gradlew clean build publish -Partifactory_url=$artifactoryUrl -Partifactory_user=$artifactoryUsername -Partifactory_password=$artifactoryPassword"
